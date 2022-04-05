6f01cb25078151ae4bb1eefc592749f69b639a9c
psixp <pablo_six@live.com>
2022-04-04 22:00:47 -0300

    novas rotas e estrutura de pastas

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path')
 // respond with "hello world" when a GET request is m to the homepage
-ade
 
 let user = [{
     id: 1,
@@ -17,23 +16,24 @@ let user = [{
     peso: 65
 }]
 
-app.post("/usuario", (req, res) => {
-   res.send(user)
+app.post("/usuario/", (req, res) => {
+   res.send("sou o post")
 })
 
-app.get('/usuario', function (req, res) {
-    res.send(user)
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
 
-app.put("/usuario/:id", (req, res) => {
-    res.send(newUser.name = "Joao")
+app.put("/usuario/", (req, res) => {
+    res.send("sou o put")
 })
 
-app.delete("/usuario/:id", (req, res) => {
-    res.send(user)
+app.delete("/usuario/", (req, res) => {
+    res.send("sou o delete")
 })
 
-app.listen(8000, () => {
+const port = 8000
+app.listen(port, () => {
     console.log("Servidor Pronto com Express")
 })
